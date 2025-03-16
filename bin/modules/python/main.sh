dc6b7d6f37111569e0151edf0f8f1adebc9f4637
joaoguilhermealvessantos <joaoguilhermeas453@gmail.com>
2025-03-16 00:29:59 -0300

    a

---
+++ bin/modules/python/main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/bin/bash
+
+cmd_python_print() {
+    echo "$@"
+}
+
+cmd_python_run() {
+    python3 -c "$@"
+}
--- bin/paradise.sh
+++ bin/paradise.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+BASE_DIR="$(dirname "$0")"
+
+MODULE="${1?'Usage: paradise <module> <subcommand> [args...]'}"
+SUBCOMMAND="${2?'Usage: paradise <module> <subcommand> [args...]'}"
+shift 2
+
+MODULE_SCRIPT="$BASE_DIR/modules/$MODULE/$MODULE.sh"
+
+[ -f "$MODULE_SCRIPT" ] || { echo "Error: Module '$MODULE' not found!"; exit 1; }
+
