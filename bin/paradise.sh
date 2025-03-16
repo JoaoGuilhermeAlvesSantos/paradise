0d2a60a7f6b7688f89993d5d039d04faf8f59bd8
joaoguilhermealvessantos <joaoguilhermeas453@gmail.com>
2025-03-16 00:30:55 -0300

    a

--- bin/paradise.sh
+++ bin/paradise.sh
@@ -6,7 +6,7 @@ MODULE="${1?'Usage: paradise <module> <subcommand> [args...]'}"
 SUBCOMMAND="${2?'Usage: paradise <module> <subcommand> [args...]'}"
 shift 2
 
-MODULE_SCRIPT="$BASE_DIR/modules/$MODULE/$MODULE.sh"
+MODULE_SCRIPT="$BASE_DIR/modules/$MODULE/main.sh"
 
 [ -f "$MODULE_SCRIPT" ] || { echo "Error: Module '$MODULE' not found!"; exit 1; }
 
